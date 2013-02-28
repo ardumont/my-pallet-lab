9989108062c322e70dde6252335bb59d897e10fd
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-02-28 17:56:21 +0100

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject my-pallet-stevedore-lab "1.0.0-SNAPSHOT"
+  :description "My pallet learning playground"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.cloudhoist/pallet "0.7.3"]]
+  :dev-dependencies [[midje      "1.4.0"]]
+  :repositories {"sonatype"           "http://oss.sonatype.org/content/repositories/releases/"
+                 "sonatype-snapshots" "http://oss.sonatype.org/content/repositories/snapshots/"})
---
+++ test/my_pallet_lab/core_test.clj
@@ -0,0 +1,7 @@
+(ns my-pallet-lab.core-test
+  (:use clojure.test
+        my-pallet-lab.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
