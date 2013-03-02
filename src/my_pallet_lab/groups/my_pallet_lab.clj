35981a45e8701c4cd069419384c0cc110315c6be
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-03-02 17:43:06 +0100

    Starting and stopping node with the ec2 provider.

--- src/my_pallet_lab/groups/my_pallet_lab.clj
+++ src/my_pallet_lab/groups/my_pallet_lab.clj
@@ -1,22 +1,50 @@
 (ns my-pallet-lab.groups.my-pallet-lab
-    "Node definitions for my-pallet-lab"
-    (:use
-     [pallet.core :only [group-spec server-spec node-spec]]
-     [pallet.crate.automated-admin-user :only [automated-admin-user]]
-     [pallet.phase :only [phase-fn]]))
+  "Node definitions for my-pallet-lab"
+  (:use
+   [pallet.core :only [group-spec server-spec node-spec converge]]
+   [pallet.crate.automated-admin-user :only [automated-admin-user]]
+   [pallet.phase :only [phase-fn]])
+  (:require
+   [pallet.action.package :as pa]
+   [pallet.configure      :as pc]))
+
+;; ami
+;; :image-id "ami-2861685c" - ;; ubuntu 12.10 eu-west-1 i386 instance-store
+;; :image-id "ami-7e636a0a" - ;; ubuntu 12.10 eu-west-1 i386 ebs
+
+;; define the machine
 
 (def
   ^{:doc "Defines a group spec that can be passed to converge or lift."}
-  my-pallet-lab
+  mygroup
   (group-spec
-   "my-pallet-lab"
-   :extends [base-server]
+   "mygroup"
    :phases {:bootstrap (phase-fn (automated-admin-user))
-            :configure (ph/phase-fn
+            :configure (phase-fn
                         (pa/package "curl")
                         (pa/package "wget")
-                        (pa/package "git")
-                        (pa/package "emacs24"))}
+                        ;; (pa/package "git")
+                        ;; (pa/package "emacs24")
+                        )}
    :node-spec (node-spec
-               :image {:os-family :ubuntu :os-version-matches "12.10"}
+               :image {:os-family :ubuntu
+                       ;; :os-version-matches "12.10"
+                       :image-id "ami-2861685c"}
                :hardware {:min-cores 1 :min-ram 512})))
+
+(defn start-node
+  "Starting the group-spec with one node"
+  [group-spec]
+  (-> group-spec
+      (assoc-in [:count] 1)
+      (converge :compute (pc/compute-service :aws))))
+
+(defn stop-node
+  "Stopping the group-spec"
+  [group-spec]
+  (-> group-spec
+      (assoc-in [:count] 0)
+      (converge :compute (pc/compute-service :aws))))
+
+(comment
+  (start-node mygroup))
