215085ea44efb7a654fca4ca59ce0d5d9ebf195d
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-03-02 16:35:57 +0100

    Simplifying using one group fits all

--- src/my_pallet_lab/core.clj
+++ src/my_pallet_lab/core.clj
@@ -10,32 +10,47 @@
   {:access-key (env/env :aws-access-key)
    :secret-key (env/env :aws-secret-key)})
 
-;; node specification (to start a node via a provider service)
-;; An empty :image specification implies a default image will be used for the nodes, usually the latest Ubuntu version, or CentOS if no Ubuntu images are available.
-;; An empty :hardware specification implies the smallest hardware configuration will be used.
+(comment
 
-(def mynodes
-  (p/node-spec
-   :image    {:os-family :ubuntu :os-version-matches "12.10"}
-   :hardware {:min-cores 1 :min-ram 512}
-   :network  {:inbound-ports [22 80]}))
+  ;; node specification (to start a node via a provider service)
+  ;; An empty :image specification implies a default image will be used for the nodes, usually the latest Ubuntu version, or CentOS if no Ubuntu images are available.
+  ;; An empty :hardware specification implies the smallest hardware configuration will be used.
 
-;; server specification (define a set of configuration to apply to the machine - permit to add phase to run during the lift or converge operations)
+  (def mynodes
+    (p/node-spec
+     :image    {:os-family :ubuntu :os-version-matches "12.10"}
+     :hardware {:min-cores 1 :min-ram 512}
+     :network  {:inbound-ports [22 80]}))
 
-(def with-wget
-  (p/server-spec
-   :phases {:configure (ph/phase-fn
-                        (pa/package "wget"))}))
+  ;; server specification (define a set of configuration to apply to the machine - permit to add phase to run during the lift or converge operations)
 
-(def with-curl
-  (p/server-spec
-   :phases {:configure (ph/phase-fn
-                        (pa/package "curl"))}))
+  (def with-wget
+    (p/server-spec
+     :phases  {:configure (ph/phase-fn
+                           (pa/package "wget"))}))
 
-;; group-spec (map node spec to their server-spec)
+  (def with-curl
+    (p/server-spec
+     :phases {:configure (ph/phase-fn
+                          (pa/package "curl"))}))
 
-(def mygroup
+  ;; group-spec (map node spec to their server-spec)
+
+  (def mygroup
     (p/group-spec
      "mygroup"
-     :extends [with-wget with-curl]
-     :node-spec mynodes))
+     :extends [with-curl with-wget]
+     :node-spec my-nodes)))
+
+(def mygroup
+  (p/group-spec
+   "mygroup"
+   :phases {:configure (ph/phase-fn
+                        (pa/package "curl")
+                        (pa/package "wget")
+                        (pa/package "git")
+                        (pa/package "emacs24"))}
+   :node-spec (p/node-spec
+               :image    {:os-family :ubuntu :os-version-matches "12.10"}
+               :hardware {:min-cores 1 :min-ram 512}
+               :network  {:inbound-ports [22 80]})))
