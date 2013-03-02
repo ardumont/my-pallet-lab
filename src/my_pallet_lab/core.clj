b8d284fdd481d4daa0a85f49892941a27b661da2
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-03-02 18:46:40 +0100

    Adding a reworked sample of output from the repl.

--- src/my_pallet_lab/core.clj
+++ src/my_pallet_lab/core.clj
@@ -64,3 +64,77 @@
   (pallet.core/converge
    (pallet.core/group-spec "mygroup" :count 1)
    :compute (pallet.compute/service "aws")))
+
+
+(comment :some-output-reworked-to-remove-list-of-possible-nodes-and-to-be-able-to-read-what-is-going-on
+         {:selected-nodes ()
+          :group {:servers ()
+                  :count 1
+                  :image {:os-family :ubuntu
+                          :image-id "us-west-1/ami-2861685c"}
+                  :phases {:bootstrap "#<automated_admin_user$automated_admin_user pallet.crate.automated_admin_user$automated_admin_user@7c14dcba>"
+                           :configure "#<my_pallet_lab$fn__9662 my_pallet_lab.groups.my_pallet_lab$fn__9662@25703b9b>"}
+                  :hardware {:min-cores 1
+                             :min-ram 512}
+                  :group-name :mygroup}
+          :node-set [{:count 1
+                      :image {:os-family :ubuntu
+                              :image-id "us-west-1/ami-2861685c"}
+                      :phases {:bootstrap "#<automated_admin_user$automated_admin_user pallet.crate.automated_admin_user$automated_admin_user@7c14dcba>"
+                               :configure "#<my_pallet_lab$fn__9662 my_pallet_lab.groups.my_pallet_lab$fn__9662@25703b9b>"}
+                      :hardware {:min-cores 1
+                                 :min-ram 512}
+                      :group-name :mygroup}]
+          :phase :pallet.phase/post-configure
+          :new-nodes ()
+          :environment {:blobstore nil
+                        :compute "#<JcloudsService pallet.compute.jclouds.JcloudsService@793b1fb5>"
+                        :user #pallet.utils.User{:username "tony"
+                                                 :public-key-path "/home/tony/.ssh/id_rsa.pub"
+                                                 :private-key-path "/home/tony/.ssh/id_rsa"
+                                                 :passphrase nil
+                                                 :password nil
+                                                 :sudo-password nil
+                                                 :no-sudo nil}
+                        :middleware ["#<core$translate_action_plan pallet.core$translate_action_plan@529f68e5>" "#<execute$ssh_user_credentials pallet.execute$ssh_user_credentials@7021d740>" "#<execute$execute_with_ssh pallet.execute$execute_with_ssh@38e00a5e>" "#<core$raise_on_error pallet.core$raise_on_error@5f52408b>"]
+                        :algorithms {:lift-fn "#<core$parallel_lift pallet.core$parallel_lift@fca1255>"
+                                     :converge-fn "#<core$parallel_adjust_node_counts pallet.core$parallel_adjust_node_counts@4a84336b>"
+                                     :execute-status-fn "#<core$stop_execution_on_error pallet.core$stop_execution_on_error@4f59e37e>"}}
+          :middleware ["#<core$translate_action_plan pallet.core$translate_action_plan@529f68e5>" "#<execute$ssh_user_credentials pallet.execute$ssh_user_credentials@7021d740>" "#<execute$execute_with_ssh pallet.execute$execute_with_ssh@38e00a5e>" "#<core$raise_on_error pallet.core$raise_on_error@5f52408b>"]
+          :old-nodes ()
+          :groups-new-nodes nil
+          :compute "#<JcloudsService pallet.compute.jclouds.JcloudsService@793b1fb5>"
+          :original-nodes []
+          :phase-list (:settings :configure)
+          :algorithms {:lift-fn "#<core$parallel_lift pallet.core$parallel_lift@fca1255>"
+                       :converge-fn "#<core$parallel_adjust_node_counts pallet.core$parallel_adjust_node_counts@4a84336b>"
+                       :execute-status-fn "#<core$stop_execution_on_error pallet.core$stop_execution_on_error@4f59e37e>"}
+          :blobstore nil
+          :target-id :mygroup
+          :groups ({:servers ()
+                    :count 1
+                    :image {:os-family :ubuntu
+                            :image-id "us-west-1/ami-2861685c"}
+                    :phases {:bootstrap "#<automated_admin_user$automated_admin_user pallet.crate.automated_admin_user$automated_admin_user@7c14dcba>"
+                             :configure "#<my_pallet_lab$fn__9662 my_pallet_lab.groups.my_pallet_lab$fn__9662@25703b9b>"}
+                    :hardware {:min-cores 1
+                               :min-ram 512}
+                    :group-name :mygroup})
+          :executor {:script/bash {:origin "#<execute$bash_on_origin pallet.execute$bash_on_origin@26edc0e0>"
+                                   :target "#<core$eval5776$raise__5777$fn__5778 pallet.core$eval5776$raise__5777$fn__5778@43e43267>"}
+                     :fn/clojure {:origin "#<execute$clojure_on_origin pallet.execute$clojure_on_origin@3846f00b>"
+                                  :target "#<core$eval5776$raise__5777$fn__5778 pallet.core$eval5776$raise__5777$fn__5778@39397f0a>"}
+                     :transfer/to-local {:origin "#<core$eval5776$raise__5777$fn__5778 pallet.core$eval5776$raise__5777$fn__5778@6dc386df>"
+                                         :target "#<core$eval5776$raise__5777$fn__5778 pallet.core$eval5776$raise__5777$fn__5778@3fcbd2c5>"}
+                     :transfer/from-local {:origin "#<core$eval5776$raise__5777$fn__5778 pallet.core$eval5776$raise__5777$fn__5778@5e7aa43b>"
+                                           :target "#<core$eval5776$raise__5777$fn__5778 pallet.core$eval5776$raise__5777$fn__5778@45b44df2>"}}
+          :groups-old-nodes nil
+          :all-nodes ()
+          :user #pallet.utils.User{:username "tony"
+                                   :public-key-path "/home/tony/.ssh/id_rsa.pub"
+                                   :private-key-path "/home/tony/.ssh/id_rsa"
+                                   :passphrase nil
+                                   :password nil
+                                   :sudo-password nil
+                                   :no-sudo nil}
+          :target-type :node})
