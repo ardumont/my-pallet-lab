19ddd2eec56401d607ce21e5aceb2af6d648afde
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-03-02 15:28:06 +0100

    Adding environ to manage system/lein variables.

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject my-pallet-stevedore-lab "1.0.0-SNAPSHOT"
   :description "My pallet learning playground"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.cloudhoist/pallet "0.7.3"]]
+                 [org.cloudhoist/pallet "0.7.3"]
+                 [environ "0.3.0"]]
   :dev-dependencies [[midje      "1.4.0"]]
   :repositories {"sonatype"           "http://oss.sonatype.org/content/repositories/releases/"
                  "sonatype-snapshots" "http://oss.sonatype.org/content/repositories/snapshots/"})
