9d2d0b1889a66eecf57d3d5398771e3e2b2129e7
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-03-03 19:34:50 +0100

    Provision node using vmfest

--- src/my_pallet_lab/groups/vmfest.clj
+++ src/my_pallet_lab/groups/vmfest.clj
@@ -8,69 +8,27 @@
    [pallet.configure               :as pc]
    [my-pallet-lab.groups.provision :as pr]))
 
-(comment ;; pre-requisite to download the image (called model in vmfest nomenclatura)
-  (def vmfest-service (pc/compute-service "vmfest" nil nil))
+(def
+  ^{:doc "Defines a group spec that can be passed to converge (creation/termination) or lift (updates)."}
+  mygroup-ec2
+  (group-spec
+   "mygroup"
+   :phases {:bootstrap (plan-fn (automated-admin-user))
+            :configure (plan-fn
+                        (package "curl"))}
+   :node-spec (node-spec
+               :image {:os-family :debian
+                       :image-id :debian-6.0.2.1-64bit-v0.3}
+               :hardware {:smallest true
+                          :min-cores 1
+                          :min-ram 512})))
 
-  ;; (pallet.compute.vmfest/add-image vmfest-service "https://s3.amazonaws.com/vmfest-images/debian-6.0.2.1-64bit-v0.3.vdi.gz")
-  ;; download both the vdi.gz and .meta file from the server
-  (pallet.compute.vmfest/add-image vmfest-service "/home/tony/Downloads/pallet/ubuntu-12.04.vdi")
+(comment
+  ;; personal function:
+  ;; - to start a node
+  (pr/start-node mygroup-ec2 :vmfest)
+  ;; - to stop one
+  (pr/stop-cluster mygroup-ec2 :vmfest)
 
-  {:ubuntu-12.04 {:os-type-id "Ubuntu_64"
-                  :sudo-password "vmfest"
-                  :no-sudo false
-                  :packager :apt
-                  :username "vmfest"
-                  :os-family :ubuntu
-                  :os-version "12.04"
-                  :uuid "/home/tony/.vmfest/models/vmfest-ubuntu-12.04.vdi"
-                  :os-64-bit true
-                  :password "vmfest"
-                  :description "Ubuntu 12.04 (64bit)"}}
-
-  (pallet.compute.vmfest/add-image vmfest-service "/home/tony/Downloads/pallet/debian-6.0.2.1-64bit-v0.3.vdi")
-
-  {:debian-6.0.2.1-64bit-v0.3 {:os-type-id "Debian_64"
-                               :sudo-password "vmfest"
-                               :no-sudo false
-                               :username "vmfest"
-                               :os-family :debian
-                               :os-version "6.0.2.1"
-                               :uuid "/home/tony/.vmfest/models/vmfest-debian-6.0.2.1-64bit-v0.3.vdi"
-                               :os-64-bit true
-                               :password "vmfest"
-                               :description "Debian 6.0.2.1 (64bit) v0.3"}
-   :ubuntu-12.04 {:os-type-id "Ubuntu_64"
-                  :sudo-password "vmfest"
-                  :no-sudo false
-                  :packager :apt
-                  :username "vmfest"
-                  :os-family :ubuntu
-                  :os-version "12.04"
-                  :uuid "/home/tony/.vmfest/models/vmfest-ubuntu-12.04.vdi"
-                  :os-64-bit true
-                  :password "vmfest"
-                  :description "Ubuntu 12.04 (64bit)"}}
-
-  (pallet.compute/images vmfest-service)
-
-  {:debian-6.0.2.1-64bit-v0.3 {:os-type-id "Debian_64"
-                               :sudo-password "vmfest"
-                               :no-sudo false
-                               :username "vmfest"
-                               :os-family :debian
-                               :os-version "6.0.2.1"
-                               :uuid "/home/tony/.vmfest/models/vmfest-debian-6.0.2.1-64bit-v0.3.vdi"
-                               :os-64-bit true
-                               :password "vmfest"
-                               :description "Debian 6.0.2.1 (64bit) v0.3"}
-   :ubuntu-12.04 {:os-type-id "Ubuntu_64"
-                  :sudo-password "vmfest"
-                  :no-sudo false
-                  :packager :apt
-                  :username "vmfest"
-                  :os-family :ubuntu
-                  :os-version "12.04"
-                  :uuid "/home/tony/.vmfest/models/vmfest-ubuntu-12.04.vdi"
-                  :os-64-bit true
-                  :password "vmfest"
-                  :description "Ubuntu 12.04 (64bit)"}})
+  ;; to list the nodes
+  (pr/pallet.compute/nodes (pc/compute-service :vmfest)))
